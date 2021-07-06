5a5909a7d5bd5147fa1860a65e782d8f59c66b09
Unknown <dipicacyx@gmail.com>
2021-07-06 17:06:02 +1000

    Trying to work with render
    Including the live folder in the repo so render can work. Maybe

---
+++ live/1.1.3/media/js/lib/dom.js
@@ -0,0 +1,1 @@
+"use strict";System.register([],function(_export,_context){"use strict";var parent;return{setters:[],execute:function(){_export("parent",parent=function parent(node,parent){parent=parent.toLowerCase();while((node=node.parentNode)!=null){if(node.nodeName.toLowerCase()===parent){return node}}return null});_export("parent",parent)}}});
